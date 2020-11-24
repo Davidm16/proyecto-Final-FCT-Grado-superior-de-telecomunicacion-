bdc5305e47ab65adedf53d567d136e938eef756c
danidany095 <62353571+danidany095@users.noreply.github.com>
2020-11-24 21:36:37 +0100

    Add files via upload

---
+++ 2_sensor_de_humedad.ino
@@ -0,0 +1,31 @@
+// 2 Senssor de Humedad de Tierra 
+int pinHumedad = A0;
+int  pinBomba_de_agua = 3;
+bool bomba_activa = false;
+
+void setup() 
+{
+  Serial.begin(6900);
+  }
+
+void loop() 
+{
+int calcular2 = map(analogRead(pinHumedad), 0, 1023, 100, 0);
+Serial.println (analogRead(pinHumedad));
+Serial.print("Porcentaje Humedad: ");
+Serial.print(calcular2);
+Serial.println(" %");
+
+if (bomba_activa == false)
+if (analogRead(pinHumedad)>=512)
+{
+ Serial.println("Poca Humedad");
+ Serial.println("Mucha Humedad");
+  digitalWrite(pinBomba_de_agua,LOW);
+  }
+  else
+  {
+    digitalWrite(pinBomba_de_agua,HIGH);
+ Serial.println("Mucha Humedad");
+    }
+  }
